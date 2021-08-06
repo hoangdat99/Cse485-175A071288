1a0a2ffd0140e0893404eaeb554944503672aa66
hoangdat99 <71364181+hoangdat99@users.noreply.github.com>
2021-08-06 16:58:41 +0700

    Update footer.php

--- web/footer/footer.php
+++ web/footer/footer.php
@@ -1,8 +1,8 @@
 <div class="infoStore">
         <div class="information">
-        <i class="fab fa-facebook"></i>
-        <i class="fab fa-instagram-square"></i>
-        <i class="fab fa-twitter-square"></i>
+          <i class="fab fa-facebook"></i>
+          <i class="fab fa-instagram-square"></i>
+          <i class="fab fa-twitter-square"></i>
         </div>
-      </div>
-    </div>
+        <p style="text-align:center">All rights reserved. Design By HD</p>
+</div>
