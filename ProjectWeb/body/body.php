36f1853b87af02a3e7e8c72a57778386a0a1f6ca
hoangdat99 <71364181+hoangdat99@users.noreply.github.com>
2021-08-10 10:42:07 +0700

    Fontend-Home

---
+++ ProjectWeb/body/body.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/5f58258f46.js"crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css"/>
+    <title>Document</title>
+</head>
+<body>
+    <div class="slide-show">
+        <img src="./images/slide1.jpg" class="imageSlideShow">
+        <img src="./images/slide2.jpg" class="imageSlideShow">
+        <img src="./images/slide3.jpg" class="imageSlideShow">
+        <img src="./images/slide4.jpg" class="imageSlideShow">
+        <img src="./images/slide5.jpg" class="imageSlideShow">
+    </div>
+    <div class="container-fluid">
+        <div class="row information">
+            <div class="col-md-2">
+                <i class="fas fa-file-alt"></i>
+                <a href="#">Văn bản pháp quy</a>
+            </div>
+            <div class="col-md-2">
+                <i class="fas fa-headset"></i>
+                <a href="#">Dịch vụ trực tuyến</a>
+            </div>
+            <div class="col-md-2">
+                <i class="fas fa-envelope-open-text"></i>
+                <a href="#">Hỗ trợ CNTT</a>
+            </div>
+            <div class="col-md-2">
+            <i class="fas fa-place-of-worship"></i>
+            <a href="#">Đảng-Đoàn thể</a>
+            </div>
+            <div class="col-md-2">
+                <i class="fas fa-torii-gate"></i>
+                <a href="#">Virtual Tour</a>
+            </div>
+        </div>
+        <div class="section-title">
+            <div class="section-title-content">
+                <h2>TIN TUYỂN SINH</h2>
+                <hr class="hr1">
+                <hr class="hr2">
+            </div>
+            <div class="section-title-title">
+                Danh sách thông tin tuyển sinh mới nhất của Trường Đại học Phenikaa
+            </div>
+        </div>  
+        <div class="row newsAdmissions">
+            <div class="col-md-6 news1">
+                <hr class="hrThumbnail1">
+                <a href="#" class="contentThumbnail1">Công bố ngưỡng điểm nhận hồ sơ xét tuyển Đại học hệ chính quy theo phương thức xét điểm thi tốt nghiệp THPT năm 2021</a>
+                <a href="#" class="timeThumbnail1"><i class="fas fa-arrow-right"></i> 06, tháng 8,2021</a>
+                <img src="./images/thumbnail1.jpg" class="imageThumbnail">
+            </div>
+            <div class="col-md-3 news2">
+                <img src="./images/thumbnail2.jpg" class="imageThumbnail2">
+                <div class="main-thumbnail">
+                    <hr class="hrThumbnail2">
+                    <a href="#" class="contentThumbnail2">Thông báo nhập học đợt 1 năm 2021</a><br><br>
+                    <a href="#" class="timeThumbnail2"><i class="fas fa-arrow-right"></i> 01, tháng 8, 2021</a>
+                </div>
+            </div>
+            <div class="col-md-3 news2">
+                <img src="./images/thumbnail3.jpg" class="imageThumbnail2">
+               <div class="main-thumbnail">
+                    <hr class="hrThumbnail2">
+                    <a href="#" class="contentThumbnail2">Thí sinh cần làm gì khi biết điểm thi tốt nghiệp THPT?</a><br><br>
+                    <a href="#" class="timeThumbnail2"><i class="fas fa-arrow-right"></i> 01, tháng 8, 2021</a>
+               </div>
+            </div>
+        </div>
+        <div class="row newsAdmissions">
+            <div class="col-md-3 news2">
+                <img src="./images/thumbnail4.jpg" class="imageThumbnail2">
+                <div class="main-thumbnail">
+                    <hr class="hrThumbnail3">
+                    <a href="#" class="contentThumbnail3">Công thức và cách tính điểm xét tốt nghiệp THPT 2021 nhanh nhất, chính xác nhất</a><br><br><br>
+                    <a href="#" class="timeThumbnail3"><i class="fas fa-arrow-right"></i> 20, tháng 7, 2021</a>
+                </div>
+            </div>
+            <div class="col-md-3 news2">
+                <img src="./images/thumbnail5.jpg" class="imageThumbnail2">
+               <div class="main-thumbnail">
+                    <hr class="hrThumbnail3">
+                    <a href="#" class="contentThumbnail3"> Tất tần tật về điều chỉnh nguyện vọng xét tuyển Đại học năm 2021 </a><br><br>
+                    <a href="#" class="timeThumbnail3"><i class="fas fa-arrow-right"></i> 19, tháng 7, 2021</a>
+               </div>
+            </div>
+            <div class="col-md-6 news1">
+                <hr class="hrThumbnail4">
+                <a href="#" class="contentThumbnail4">Tuyển sinh đào tạo trình độ Thạc sĩ ngành Dược lý và Dược lâm sàng đợt 1 năm 2021</a>
+                <a href="#" class="timeThumbnail4"><i class="fas fa-arrow-right"></i> 06, tháng 7, 2021</a>
+                <img src="./images/thumbnail6.jpg" class="imageThumbnail4">
+            </div>
+        </div>
+        <div class="col-md-12 btn-more">
+            <button class="more">Xem Thêm</button>
+        </div> 
+    </div>
+    <div class="event-title">
+        <div class="event-title-content">
+            <h2>SỰ KIỆN</h2>
+            <hr class="hr3">
+            <hr class="hr4">
+            <p>Các sự kiện đã, đang và sắp diễn ra tại Trường Đại học Phenikaa</p>
+        </div>
+        <div class="slideshows-event">
+            <div class="slideshow-item">
+                <div class="post-image">
+                    <img src="./images/imgEvent1.jpg" class="img-event">
+                </div>
+                <div class="post-text">
+                    <div class="post-date">
+                        <div class="post-day">15</div>
+
+                        <div class="post-month">Tháng 6</div>
+                    </div>
+                    <a href="#"><p>Kế hoạch tổ chức khóa đào tạo  “Kỹ năng thích ứng môi trường làm việc” </p></a>
+                    
+                </div>
+            </div>
+            <div class="slideshow-item">
+                <div class="post-image">
+                    <img src="./images/imgEvent2.jpg" class="img-event">
+                </div>
+                <div class="post-text">
+                    <div class="post-date">
+                        <div class="post-day">20</div>
+                       
+                        <div class="post-month">Tháng 5</div>
+                    </div>
+                    <a href="#"><p>Cuộc Tiktok Challenge: Phenikaar Anti Covid đồng hành cùng chiến dịch "Tuổi trẻ Phenikaa: Học online - Trổ tài chống dịch"  </p></a>
+                    
+                </div>
+            </div>
+            <div class="slideshow-item">
+                <div class="post-image">
+                    <img src="./images/imgEvent4.jpg" class="img-event">
+                </div>
+                <div class="post-text">
+                    <div class="post-date">
+                        <div class="post-day">20</div>
+                       
+                        <div class="post-month">Tháng 5</div>
+                    </div>
+                    <a href="#"><p>Tuổi trẻ Phenikaa: Học online – Trổ tài chống dịch</p></a>
+                    
+                </div>
+            </div>
+            <div class="slideshow-item">
+                <div class="post-image">
+                    <img src="./images/imgEvent3.jpg" class="img-event">
+                </div>
+                <div class="post-text">
+                    <div class="post-date">
+                        <div class="post-day">13</div>
+                      
+                        <div class="post-month">Tháng 5</div>
+                    </div>
+                    <a href="#"><p>Khóa đào tạo “Kỹ năng chinh phục nhà tuyển dụng” </p></a>
+                    
+                </div>
+            </div>
+            <div class="slideshow-item">
+                <div class="post-image">
+                    <img src="./images/imgEvent5.jpg" class="img-event">
+                </div>
+                <div class="post-text">
+                    <div class="post-date">
+                        <div class="post-day">23</div>
+                        <div class="post-month">Tháng 4</div>
+                    </div>
+                    <a href="#"><p>Cuộc thi TikTok Challenge: Phenikaa Uni: Right choice - Bright future</p></a>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 btn-more">
+            <button class="more">Xem thêm</button>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row notification">
+            <div class="col-md-12 lg-12">
+                <h2>TIN TỨC-THÔNG BÁO</h2>
+                <hr class="hr1">
+                <hr class="hr2">
+                <p>Tin tức mới nhất về các hoạt động của Trường Đại học Phenikaa</p>
+            </div>
+            <div class="col-md-5 lg-5 left">
+                <div class="image-notification">
+                    <img src="./images/imgNotification1.jpg" class="imageNotification">
+                    <div class="time">
+                        <div class="day">05</div>
+                        <div class="month">Tháng 7</div>
+                    </div>
+                </div>
+                <div class="notification-content">
+                    <a href="#">Thông báo Kết quả xét tuyển Đại học hệ chính quy (theo phương thức xét tuyển thẳng và kết quả học tập bậc THPT - Đợt 1 năm</a>
+                    <hr class="hrNotification1">
+                    <div class="post-description">
+                        <div class="discription">
+                        Hội đồng tuyển sinh Trường Đại học Phenikaa (PKA) thông báo kết quả xét tuyển đại học.
+                        </div>
+                        <button class="link"><i class="fas fa-arrow-right">xem thêm</i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 lg-5 right">
+            <div class="image-notification">
+                    <img src="./images/imgNotification2.jpg" class="imageNotification">
+                    <div class="time">
+                        <div class="day">05</div>
+                        <div class="month">Tháng 8</div>
+                    </div>
+                </div>
+                <div class="notification-content">
+                    <a href="#">Trường Đại học Phenikaa công bố ngưỡng điểm nhận hồ sơ xét tuyển Đại học hệ chính quy theo phương thức xét điểm thi tốt nghiệp THPT năm 2021</a>
+                    <hr class="hrNotification1">
+                    <div class="post-description">
+                        <div class="discription">
+                        Hội đồng Tuyển sinh Trường Đại học Phenikaa (PKA) công bố ngưỡng điểm nhận hồ sơ đăng ký xét tuyển (điểm sàn) đại học
+                        </div>
+                        <button class="link"><i class="fas fa-arrow-right">xem thêm</i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row notification">
+            <div class="col-md-5 lg-5 left">
+                <div class="image-notification">
+                    <img src="./images/imgNotification3.png" class="imageNotification">
+                    <div class="time">
+                        <div class="day">09</div>
+                        <div class="month">Tháng 8</div>
+                    </div>
+                </div>
+                <div class="notification-content">
+                    <a href="#">Tập huấn trực tuyến dành cho sinh viên Một sức khỏe với chủ đề “Một sức khỏe và Truyền thông nguy cơ phòng chống</a>
+                    <hr class="hrNotification1">
+                    <div class="post-description">
+                        <div class="discription">
+                        Trong 5 tuần liên tiếp, Mạng lưới Một sức khỏe các trường đại học Việt Nam tổ chức chuỗi hội thảo trực tiếp với chủ đề “Một sức khỏe và Truyền thông nguy cơ phòng chống COVID-19”
+                        </div>
+                        <button class="link"><i class="fas fa-arrow-right">xem thêm</i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 lg-5 right">
+            <div class="image-notification">
+                    <img src="./images/imgNotification4.jpg" class="imageNotification">
+                    <div class="time">
+                        <div class="day">05</div>
+                        <div class="month">Tháng 8</div>
+                    </div>
+                </div>
+                <div class="notification-content">
+                    <a href="#">Thông báo nhập học đợt 1 năm 2021</a>
+                    <hr class="hrNotification1">
+                    <div class="post-description">
+                        <div class="discription">
+                        Thí sinh đã đạt kết quả xét tuyển đại học hệ chính quy các ngành/chương trình đào tạo (CTĐT) theo phương thức tuyển thẳng và kết quả học tập bậc THPT (học bạ) đợt 1 năm 2021.
+                        </div>
+                        <button class="link"><i class="fas fa-arrow-right">xem thêm</i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 btn-more">
+            <button class="more">Xem Thêm</button>
+        </div> 
+    </div>
+
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
+<script src="./javascript/javascript.js"></script>
+</body>
+</html> 
---
+++ ProjectWeb/javascript/javascript.js
@@ -0,0 +1,27 @@
+// *****js for slide show 1 ****
+var myIndex = 0;
+carousel();
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("imageSlideShow");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+// *********************
+
+$(document).ready(function(){
+  $('.slideshows-event').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true
+  });
+});
