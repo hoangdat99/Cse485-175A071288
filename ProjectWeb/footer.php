960c7f360af8ca6f60e2a6bbddf2985484393864
Tranthaiduong <68003159+Tranthaiduong@users.noreply.github.com>
2021-08-19 15:52:58 +0700

    Add files via upload

---
+++ ProjectWeb/footer.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+  <head>
+	<title>Trang chủ- Trường Đại học PHENIKAA</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="phenika/school.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/school.css">
+  </head>
+
+
+    <!-- Footer Start -->
+  <footer class="text-center text-lg-start">
+      <section class="p-4" style="background-color: #003366;">
+        <div class="footer container">
+          <div class="text-left">
+            <img src="imagesĐH/logoft.png" alt="Logo Footer">
+          </div>
+
+          <div class="row mt-3 small text-left">
+            <div class="col-md-4 col-lg-3 col-xl-3 text-light">
+              <p>
+            <a class="fas fa-home me-3"></a> Yên Nghĩa - Hà Đông - Hà Nội
+              </p>
+              <p>
+            <a class="fas fa-phone me-3"></a>024.62918118 | Hotline: 094.651.1010  
+              </p>
+              <p>
+            <a class="fas fa-envelope me-3"></a>Info@phenikaa-uni.edu.vn
+              </p>
+            </div>
+        
+
+            <div class="col-md-3 col-lg-2 col-xl-2 text-light">
+              <p>
+                <a href="#!" class="text-reset">Ba công khai</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Tra cứu văn bằng</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Văn bản</a>
+              </p>
+            </div>
+
+
+            <div class="col-md-4 col-lg-3 col-xl-3 text-light">
+              <p>
+                <a href="#!" class="text-reset">Bài báo ISI/SCOPUS</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Sáng chế/ Giải pháp hữu ích</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Tạp chí khoa học</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Quỹ Khoa học-Công nghệ</a>
+              </p>
+            </div>
+          
+            <div class="col-md-3 col-lg-2 col-xl-2 text-light">
+              <p>
+                <a href="#!" class="text-reset">Khoa-Trường</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Viện-Trung tâm</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Phòng-Ban</a>
+              </p>
+            </div>
+      
+            <div class="col-md-3 col-lg-2 col-xl-2 text-light">
+              <p>
+                <a href="#!" class="text-reset">Liên hệ</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Tuyển dụng</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Trải nghiệm</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Sơ đồ trang</a>
+              </p>
+            </div>
+          </div>
+          <div class="map">
+            <div class="row">
+              <div class="">
+                <div class="col-9 mb-3 text-left">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14903.130589477889!2d105.74747300000001!3d20.961242!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x391a39d4325be464!2zxJDhuqFpIGjhu41jIFBIRU5JS0FB!5e0!3m2!1svi!2sus!4v1628800948470!5m2!1svi!2sus" width= 100% height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+
+      <!-- Copyright -->
+        <div class="Coppyright mt-4">
+          <div class="text-left text-light small">
+            <p class="font-italic">
+              Coppyright © 2021 Trường Đại học PHENIKAA
+            </p>
+          </div>
+        </div>  
+    <!-- Copyright -->
+        </div>
+      </section>
+    <!-- Section: Links  -->
+  </footer>
+      <!-- Footer End -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+  
+</html>
